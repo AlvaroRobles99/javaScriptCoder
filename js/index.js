2841a1b95a0b2ec7215b0bc4f5ff0fa1a4b41dba
Alvaro Robles <alvaroagusrobles@gmail.com>
2022-10-03 02:54:57 -0300

    segundo commit la entrega con ciclo y if

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
-let nombre = "agustin";
+const datoUsuarioGuardado = "Agus99"
+const datoContraseñaGuardado = "Contraseña123"
+
+let pregunta = confirm("¿Desea ingresar con un usuario ya creado?")
+
+ if(pregunta === true){
+    login()
+}
+
+ function login(){
+    let nombreUsuario = prompt("Ingrese su nombre de usuario")
+    let contraseñaUsuario = prompt("Ingrese contraseña")
+    if(nombreUsuario === datoUsuarioGuardado && contraseñaUsuario === datoContraseñaGuardado){
+       bienvenida(nombreUsuario)
+    } else {
+       let nuevoIntento = confirm("El usuario o la contraseña no es valido. ¿Desea intentarlo de nuevo?")
+       if(nuevoIntento === true) {
+        login()
+       }
+
+    }
+}
+
+function bienvenida(){
+    alert("Bienvenido " + datoUsuarioGuardado)
+}
+
+
+let preguntaTienda = confirm("¿Quiere comprar chocolate?")
+if(preguntaTienda === true){
+    tienda()
+}
+function tienda(){
+    do{
+        preguntarCompra = parseInt(prompt("¿cuatos desea comprar?"))
+        respuestaSeguirComprando = confirm("¿desea cambiar la cantidad?")
+   }
+   while(respuestaSeguirComprando === true){
+    alert("gracias por su compra")
+   }
+  
+}
