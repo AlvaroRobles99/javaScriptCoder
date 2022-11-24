8e71b8579303c7c9bf743947328787feac8c6abf
Alvaro Robles <alvaroagusrobles@gmail.com>
2022-11-24 05:12:30 -0300

    css and carritoDeCompra

--- js/tienda.js
+++ js/tienda.js
@@ -8,13 +8,19 @@ const botonNvidiaF = document.querySelector("button.botonNvidiaF")
 const botonMayorF = document.querySelector("button.botonMayorF")
 const botonMenorF = document.querySelector("button.botonMenorF")
 const botonVerTodo = document.querySelector("button.botonVerTodo")
+const iconCarrito = document.querySelector("div.iconCarrito")
+const carritoCargar = document.querySelector("article.tienda")
+const contenedorProductosCarritos = document.querySelector("div.carrito")
+const main = document.querySelector("main.main")
 const plantillaProducto = (tarjeta) => {
     return ` <div class="tarjetasGraficas">
               <h4>${tarjeta.nombre}</h4>
               <img loading="lazy" src="${tarjeta.imagen}" alt="${tarjeta.nombre}">
               <p>$${tarjeta.precio}</p>
-              <button class="botonAddCarrito" id="${tarjeta.id}">AGREGAR A CARRITO</button>
+              <button class="botonAddCarrito" id="${tarjeta.id}">SUMAR AL CARRITO</button>
              </div>`}
+                  
+                         
 const agregarAlCarritoAMD = (btnId) => {
     if (btnId <= 7) {
         let tarjetaSumar = tarjetasVideoAMD.find(tarjeta => tarjeta.id == btnId)
@@ -108,3 +114,40 @@ const filtrarMayor = () =>{
     cargarTiendaPreciosMayor()
     botonesSumarCarrito()
 }
+const productisCarritoCargar = ()=>{}
+
+iconCarrito.addEventListener("click", ()=>{
+       carritoCargar.innerHTML = ""
+       carritoCargar.innerHTML=`<section class="sidebarCarrito"> 
+       <h2>
+           FILTRAR
+       </h2>
+       <button class="botonVerTodo">
+           COMPRAR
+       </button>
+       <h3>
+           METODO DE PAGO
+       </h3>
+       <button class="botonAMDF">
+           PLACAS DE VIDEO AMD
+       </button>
+       <button class="botonNvidiaF">
+           PLACAS DE VIDEO NVIDIA
+       </button>
+       <h3>ENVIO</h3>
+       <button class="botonMayorF">
+           DE MAYOR A MENOR
+       </button>
+       <button class="botonMenorF">
+           DE MENOR A MAYOR
+       </button>
+   </section>`
+  
+       carrito.forEach(tarjeta => {
+        if(carrito ){
+            contenedorProductosCarritos.innerHTML +=plantillaProducto(tarjeta)
+        } }) 
+})
+
+
+
